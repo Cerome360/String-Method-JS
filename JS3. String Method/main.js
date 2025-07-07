786355055aa4477477fbda6cc50c3758ce8bf087
Cerome360 <61514307+Cerome360@users.noreply.github.com>
2025-07-07 12:26:16 +0100

    Add files via upload

---
+++ JS3. String Method/main.js
@@ -0,0 +1,14 @@
+// Strings
+const myVariable = "Mathematics";
+
+// The length property
+console.log(myVariable.charAt(0));
+console.log(myVariable.length);
+console.log(myVariable.indexOf("at"));
+console.log(myVariable.lastIndexOf("at"));
+console.log(myVariable.slice(5, 9));
+console.log(myVariable.toLowerCase());
+console.log(myVariable.toUpperCase());
+console.log(myVariable.includes("mat"));
+console.log("John, Bless, Dave".split(","));
+
